bd869feb957dcde7d091d604e6b88fad49a2e490
Vivek Gahunia <vgahunia@gmail.com>
2015-09-28 14:54:34 -0400

    Add microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
 
   attr_accessor :remember_token
 	before_save { self.email = email.downcase }
@@ -37,6 +38,10 @@ class User < ActiveRecord::Base
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
 end
